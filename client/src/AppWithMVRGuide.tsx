23a0953672c3ed7dd85a81beffcc04ff0673fb84
LordLingo <mark@precisehire.com>
2026-09-22 08:45:03 -0500

    Route continuous monitoring guide and resources feature

--- client/src/AppWithMVRGuide.tsx
+++ client/src/AppWithMVRGuide.tsx
@@ -4,8 +4,40 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
 import SiteLayout from "@/components/site/SiteLayout";
+import ContinuousBackgroundCheckMonitoringGuide from "./pages/ContinuousBackgroundCheckMonitoringGuide";
+import ResourcesWithContinuousMonitoringGuide from "./pages/ResourcesWithContinuousMonitoringGuide";
 import MVRBackgroundCheckEmployerGuide from "./pages/MVRBackgroundCheckEmployerGuide";
 import AppWithWeeklyGuides from "./AppWithWeeklyGuides";
+
+function ContinuousMonitoringGuideApp() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster richColors position="top-center" />
+          <SiteLayout>
+            <ContinuousBackgroundCheckMonitoringGuide />
+          </SiteLayout>
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+function ResourcesHubApp() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider defaultTheme="light">
+        <TooltipProvider>
+          <Toaster richColors position="top-center" />
+          <SiteLayout>
+            <ResourcesWithContinuousMonitoringGuide />
+          </SiteLayout>
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
 
 function MVRGuideApp() {
   return (
@@ -25,6 +57,8 @@ function MVRGuideApp() {
 export default function AppWithMVRGuide() {
   return (
     <Switch>
+      <Route path="/resources/continuous-background-check-monitoring-employer-guide" component={ContinuousMonitoringGuideApp} />
+      <Route path="/resources" component={ResourcesHubApp} />
       <Route path="/resources/mvr-background-checks-employer-guide" component={MVRGuideApp} />
       <Route component={AppWithWeeklyGuides} />
     </Switch>
